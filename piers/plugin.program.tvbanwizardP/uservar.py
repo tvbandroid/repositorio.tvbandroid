5dd21c97b89e7dfc2733f2c4ee5220e0f676ca1d
tvbandroid <tvbandbuild@gmail.com>
2025-03-09 20:26:13 +0100

    🛠️ - 1

--- piers/plugin.program.tvbanwizardP/uservar.py
+++ piers/plugin.program.tvbanwizardP/uservar.py
@@ -1,5 +1,5 @@
 '''#####-----Build File-----#####'''
-buildfile = 'https://raw.githubusercontent.com/tvbandroid/repositorio.tvbandroid/master/andfiles/builds.txt'
+buildfile = 'https://raw.githubusercontent.com/tvbandroid/repositorio.tvbandroid/master/buildfiles/builds.txt'
 
 '''#####-----Videos File-----#####'''
 videos_url = ''
@@ -14,7 +14,7 @@ changelog_dir  = ''
 excludes  = ['plugin.video.absolution', 'plugin.video.asgard', 'plugin.video.fenlight', 'plugin.video.free99', 'plugin.video.pov', 'plugin.video.scrubsv2', 'plugin.video.thechains', 'plugin.video.themoviedb.helper', 'repository.redwizard', 'script.module.resolveurl', 'service.subtitles.a4ksubtitles']
 
 #Change Build Names
-BUILDS = [{'Old Build': "TVban matnexompie", 'New Build': "TVban Wizard Full"},
-          {'Old Build': "TVban matnexom", 'New Build': "TVban Wizard Full"},
-          {'Old Build': "ArcticFuse TVban Amillans", 'New Build': "TVban Wizard Amillan"}]
+BUILDS = [{'Old Build': "TVban matnexompie", 'New Build': "TVban matnexompie Full"},
+          {'Old Build': "TVban matnexom", 'New Build': "TVban matnexom Full"},
+          {'Old Build': "ArcticFuse TVban Amillans", 'New Build': "TVban arcticFuse Amillan"}]
           
